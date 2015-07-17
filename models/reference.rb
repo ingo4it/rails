4061620cbf1755275f33f44aebd976adc8cf6ebf
root <ingoklein0508@gmail.com>
2015-07-17 16:18:50 +0200

    reference fixed

--- models/reference.rb
+++ models/reference.rb
@@ -1,5 +1,19 @@
+# == Schema Information
+#
+# Table name: references
+#
+#  id                 :integer          not null, primary key
+#  content            :string(255)
+#  user_id            :integer
+#  auth_id            :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  rating             :string(255)
+#  met_on_itsplatonic :boolean
+#
 
 class Reference < ActiveRecord::Base
+  
   attr_accessible :content, :auth_id, :rating, :met_on_itsplatonic
   belongs_to :user
   
