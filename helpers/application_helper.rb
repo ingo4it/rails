c770f53a28f30200780ce5603aca43187811f834
root <ingoklein0508@gmail.com>
2015-08-23 19:07:09 +0200

    added helper

---
+++ helpers/application_helper.rb
@@ -0,0 +1,14 @@
+module ApplicationHelper
+# Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "ItsPlatonic"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end  
+  # def current_user?(user)
+    # user == current_user
+  # end
+end
---
+++ helpers/badges_users_helper.rb
@@ -0,0 +1,3 @@
+module BadgesUsersHelper
+
+end
